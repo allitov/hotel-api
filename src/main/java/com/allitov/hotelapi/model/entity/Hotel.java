af4de7587bb38bc4c549cce3698da6890d40bb7b
allitov <litovkinalexan@gmail.com>
2024-03-29 17:50:48 +0700

    Add hotel entity and db table

---
+++ src/main/java/com/allitov/hotelapi/model/entity/Hotel.java
@@ -0,0 +1,48 @@
+package com.allitov.hotelapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * The class that represents an entity of a hotel.
+ * @author allitov
+ * @version 1.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    @EqualsAndHashCode.Exclude
+    private String description;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "distance_from_center")
+    @EqualsAndHashCode.Exclude
+    private Float distanceFromCenter;
+
+    @Column(name = "rating")
+    @EqualsAndHashCode.Exclude
+    private Float rating;
+
+    @Column(name = "number_of_ratings")
+    @EqualsAndHashCode.Exclude
+    private Integer numberOfRatings;
+}
