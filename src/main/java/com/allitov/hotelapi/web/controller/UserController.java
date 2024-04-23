218ef647851c14e12aa1f37f1228711c2ad8c764
allitov <litovkinalexan@gmail.com>
2024-04-23 15:03:46 +0700

    Add user controller openAPI documentation

--- src/main/java/com/allitov/hotelapi/web/controller/UserController.java
+++ src/main/java/com/allitov/hotelapi/web/controller/UserController.java
@@ -2,9 +2,18 @@ package com.allitov.hotelapi.web.controller;
 
 import com.allitov.hotelapi.model.service.UserService;
 import com.allitov.hotelapi.web.dto.request.UserRequest;
+import com.allitov.hotelapi.web.dto.response.ErrorResponse;
 import com.allitov.hotelapi.web.dto.response.UserListResponse;
 import com.allitov.hotelapi.web.dto.response.UserResponse;
 import com.allitov.hotelapi.web.mapping.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +26,29 @@ import java.net.URI;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@Tag(name = "User controller", description = "User controller API version 1.0")
 public class UserController {
 
     private final UserService userService;
 
     private final UserMapper userMapper;
 
+    @Operation(
+            summary = "Get all users",
+            description = "Get all users. Returns a list of users."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 200 and list of users if everything completed successfully.",
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = UserListResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @GetMapping
     public ResponseEntity<UserListResponse> findAll() {
         log.info("Find all users");
@@ -30,6 +56,35 @@ public class UserController {
         return ResponseEntity.ok(userMapper.entityListToListResponse(userService.findAll()));
     }
 
+    @Operation(
+            summary = "Get user by id",
+            description = "Get user by id. Returns a user with requested id.",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 200 and user if everything completed successfully.",
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = UserResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 404 and error message if user with requested id was not found.",
+                    responseCode = "404",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> findById(@PathVariable("id") Integer id) {
         log.info("Find by id request with id = '{}'", id);
@@ -37,6 +92,41 @@ public class UserController {
         return ResponseEntity.ok(userMapper.entityToResponse(userService.findById(id)));
     }
 
+    @Operation(
+            summary = "Create user",
+            description = "Create user. Returns a created user location."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 201 and created user location " +
+                            "if everything completed successfully.",
+                    responseCode = "201",
+                    headers = {
+                            @Header(name = "Location", description = "A created user location")
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 400 and error message if request has invalid values.",
+                    responseCode = "400",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 409 and error message " +
+                            "if request contains already existing username and email.",
+                    responseCode = "409",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody UserRequest request) {
         log.info("Create request with body = '{}'", request);
@@ -46,6 +136,51 @@ public class UserController {
         ).build();
     }
 
+    @Operation(
+            summary = "Update user by id",
+            description = "Update user by id. Returns status 204.",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 204 if everything completed successfully.",
+                    responseCode = "204"
+            ),
+            @ApiResponse(
+                    description = "Returns status 400 and error message if request has invalid values.",
+                    responseCode = "400",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 404 and error message " +
+                            "if user with requested id was not found.",
+                    responseCode = "404",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 409 and error message " +
+                            "if request contains already existing username and email.",
+                    responseCode = "409",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateById(@PathVariable("id") Integer id,
                                            @Valid @RequestBody UserRequest request) {
@@ -55,6 +190,19 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Delete user by id",
+            description = "Delete user by id. Returns status 204.",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 204 if everything completed successfully.",
+                    responseCode = "204"
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable("id") Integer id) {
         log.info("Delete by id request with id = '{}'", id);
--- src/main/java/com/allitov/hotelapi/web/dto/response/UserResponse.java
+++ src/main/java/com/allitov/hotelapi/web/dto/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.allitov.hotelapi.web.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +16,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserResponse {
 
+    @Schema(example = "1")
     private Integer id;
 
+    @Schema(example = "username")
     private String username;
 
+    @Schema(example = "email")
     private String email;
 
+    @Schema(example = "USER")
     private String role;
 }
