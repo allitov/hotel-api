3b0bf880f09f6a5f809b34857bc82b132813add9
allitov <litovkinalexan@gmail.com>
2024-04-24 15:03:45 +0700

    Add booking entity and db table

---
+++ src/main/java/com/allitov/hotelapi/model/entity/Booking.java
@@ -0,0 +1,41 @@
+package com.allitov.hotelapi.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * The class that represents an entity of a booking.
+ * @author allitov
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "booking")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_id", referencedColumnName = "id")
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "from")
+    private LocalDate from;
+
+    @Column(name = "to")
+    private LocalDate to;
+}
